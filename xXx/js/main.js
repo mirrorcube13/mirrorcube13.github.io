2c7f0793cc9068cbf027fddd1c2ff4684f62cb62
mirrorcube13 <koss737@mail.ru>
2018-03-08 14:59:51 +0300

    commit

--- xXx/js/main.js
+++ xXx/js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     $(window).scroll(function(){  
         if ($(window).scrollTop() > 20 ){
-            $(".navbar").css("background", "#2F2F2F");
+            $(".navbar").css("background", "rgba(47, 47, 47, 0.9)");
         }
         else {
         	$(".navbar").css("background", "transparent");
         }
     });
 });
+
+$('.carousel').carousel({
+  interval: 7000,
+  pause: "false"
+})
