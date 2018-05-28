dcd1ee4ebc15a6d47912eaf9402144cd586bcf25
mirrorcube13 <koss737@mail.ru>
2018-05-28 20:00:31 +0300

    commit

--- project_X/js/main.js
+++ project_X/js/main.js
@@ -24,3 +24,12 @@ $(document).ready(function(){
 		}
 	});
 });
+// arrow
+$(document).ready(function(){
+    $("#arrow").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
