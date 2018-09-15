71872342d1a26981d4e7faa6c4113c1b9bf6d7bf
mirrorcube13 <koss737@mail.ru>
2018-09-15 21:10:34 +0300

    commit

---
+++ for_Nick/js/main.js
@@ -0,0 +1,17 @@
+$(document).on('ready', function() {
+
+      $(".lazy").slick({
+        infinite: true,              
+        autoplay: true,
+  		  autoplaySpeed: 3000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        arrows: true,
+        fade: true,
+  		cssEase: 'linear'
+      });
+
+
+    });
+
+
