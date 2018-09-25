c4c40d7031d1bd3d3acf7383e579a9bcbec19b3c
mirrorcube13 <koss737@mail.ru>
2018-09-25 03:16:13 +0300

    commit

--- for_Nick/js/main.js
+++ for_Nick/js/main.js
@@ -7,12 +7,28 @@ $(document).on('ready', function() {
   		  autoplaySpeed: 3000,
         pauseOnHover: false,
         pauseOnFocus: false,
-        arrows: true,
         fade: true,
-  		cssEase: 'linear'
+  		  cssEase: 'linear', 
+      arrows: true,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next')
       });
-
-
     });
 
+$(document).ready(function(){
+  $(".navbar-nav").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top-50}, 1000);
+  });
+});
 
+$(document).ready(function(){
+  $(".main-block").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top-50}, 1000);
+  });
+});
