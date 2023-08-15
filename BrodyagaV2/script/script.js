bfb9e67d2cb34d5fd32a27d093055a68a769e08b
Evgeniy <32111069+mirrorcube13@users.noreply.github.com>
2023-08-15 15:01:04 +0300

    commit

--- BrodyagaV2/script/script.js
+++ BrodyagaV2/script/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         //забираем идентификатор бока с атрибута href
         var id  = $(this).attr('href'),
         //узнаем высоту от начала страницы до блока на который ссылается якорь
-            top = $(id).offset().top;
+            top = $(id).offset().top - 50;
         //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1500);
     });
