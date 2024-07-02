44f3ef056c5ef576a870d4f90b59cec0d87f31df
Evgeniy <koss737@mail.ru>
2024-07-02 22:46:46 +0300

    new_br

---
+++ Test3/script/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#arrow").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top - 50;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  // Your custom options
+  
+});
