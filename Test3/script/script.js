b8c6d37fa903c90ef5d76343a0f023ed0e4d2a5c
Evgeniy <koss737@mail.ru>
2024-08-08 00:02:10 +0300

    fix

---
+++ Test3/script/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#arrow").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top - 50;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 0);
+    });
+});
+
+
+Fancybox.bind('[data-fancybox="gallery"]', {
+  // Your custom options
+  
+});
+
+let valuesDisplays = document.querySelectorAll(".num");
+let interval = 1000;
+
+// console.log(valuesDisplays);
+
+valuesDisplays.forEach((valuesDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valuesDisplay.getAttribute
+        ("data-val"));
+
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 10;
+        valuesDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+        // body...
+    },duration)
+})
